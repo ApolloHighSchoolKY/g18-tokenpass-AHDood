24171426a126f44d6b2d72514f8f6415f170497a
AHDood <noe.almanza@stu.daviess.kyschools.us>
2026-02-04 17:54:36 +0000

    Commit

--- TokenPass.java
+++ TokenPass.java
@@ -1,9 +1,24 @@
-public TokenPass(int playerCount){
+public TokenPass (int playerCount){
   /*  to be answered in part (a) */
-  
+   
+  int token = count[playerCount];
+  count[playerCount] =0;
+
+  int i= playerCount + 1;
+
+  while (token>=0)
+  {
+    if (i >= count.length)
+    {
+      i=0;
+    }
+    count[i]++;
+    token--;
+    index++;
+  }
 }
 
 public void distributeCurrentPlayerTokens(){
   /* to be answered in part (b) */
-  
+  return count;
 }
